bc42cd859448415789310743dec6dd651d0412b8
Ragil18 <90504396+Ragil18@users.noreply.github.com>
2021-09-11 16:08:08 +0530

    Create substring.java

---
+++ substring.java
@@ -0,0 +1,7 @@
+1.	public class TestSubstring{  
+2.	 public static void main(String args[]){  
+3.	   String s="ragil immanuvel";  
+4.	   System.out.println(s.substring(6));//immanuvel 
+5.	   System.out.println(s.substring(0,6));//ragil
+6.	 }  
+7.	}  
