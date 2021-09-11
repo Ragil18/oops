770590f16d573258f00ea684eeb8c91a757b41dd
Ragil18 <90504396+Ragil18@users.noreply.github.com>
2021-09-11 16:03:33 +0530

    Create single dimensional array.java

---
+++ single dimensional array.java
@@ -0,0 +1,16 @@
+1.	class Testarray{  
+2.	public static void main(String args[]){  
+3.	  
+4.	int a[]=new int[5];//declaration and instantiation  
+5.	a[0]=101;//initialization  
+6.	a[1]=201;  
+7.	a[2]=701;  
+8.	a[3]=401;  
+9.	a[4]=501;  
+10.	  
+11.	//printing array  
+12.	for(int i=0;i<a.length;i++)//length is the property of array  
+13.	System.out.println(a[i]);  
+14.	  
+15.	}}  
+ 
