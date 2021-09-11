f95b7370225b9dd08520220af9dd5bb4a97b5084
Ragil18 <90504396+Ragil18@users.noreply.github.com>
2021-09-11 15:35:18 +0530

    Create iteration of loop.java

---
+++ iteration of loop.java
@@ -0,0 +1,12 @@
+1.	import java.util.*;  
+2.	class TestCollection2{  
+3.	 public static void main(String args[]){  
+4.	  ArrayList<String> al=new ArrayList<String>();  
+5.	  al.add("Ragil");  
+6.	  al.add("jacob");  
+7.	  al.add("varun");  
+8.	  al.add("lokesh");  
+9.	  for(String obj:al)  
+10.	    System.out.println(obj);  
+11.	 }  
+12.	}  
