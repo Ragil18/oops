fd9f8d0380846b1a6a7607e24df7b7bcd4170bd0
Ragil18 <90504396+Ragil18@users.noreply.github.com>
2021-09-12 21:06:43 +0530

    Create finalclass.java

---
+++ finalclass.java
@@ -0,0 +1,10 @@
+1.	final class car{}  
+2.	  
+3.	class suzuki extends car{  
+4.	  void run(){System.out.println("do not drink and drive");}  
+5.	    
+6.	  public static void main(String args[]){  
+7.	  suzuki1 suzuki= new suzuki();  
+8.	  suzuki.run();  
+9.	  }  
+10.	}  
