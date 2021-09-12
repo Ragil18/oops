3dc011d95cee3611aa13500f99288e05c42b351e
Ragil18 <90504396+Ragil18@users.noreply.github.com>
2021-09-12 20:50:59 +0530

    Create static binding.java

---
+++ static binding.java
@@ -0,0 +1,8 @@
+1.	class ragil{  
+2.	 private void eat(){System.out.println("ragil is eating...");}  
+3.	  
+4.	 public static void main(String args[]){  
+5.	  Dog d1=new Dog();  
+6.	  d1.eat();  
+7.	 }  
+8.	}  
